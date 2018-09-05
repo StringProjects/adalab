b469060fe87ca0f6cd63a6e12a9af1d70d199fdb
raquelv85 <raquelv85@gmail.com>
2018-09-05 12:47:36 +0200

    add icon psw

--- src/Components/LandingComponents/WeForm.js
+++ src/Components/LandingComponents/WeForm.js
@@ -14,9 +14,10 @@ class WeForm extends Component {
               </FormGroup>
               <FormGroup>
                 <Input type="password" className="form-control input--login-style" id="pwd" placeholder="Contraseña" name="pswd"/>
+                <i className="far fa-eye-slash icon--psw"></i>
               </FormGroup>
               </FormGroup>
-              <Button  type="submit" className="btn btn--login"><i class="fas fa-chevron-right"></i></Button>
+              <Button  type="submit" className="btn btn--login"><i className="fas fa-chevron-right"></i></Button>
             </Form>
       </div>
     );
