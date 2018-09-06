62738d5de8e58a99fd75a27fbbc26579e164364b
crstinapl <acpleiro@gmail.com>
2018-09-06 18:28:30 +0200

    "solve conflicts again"

--- src/public/Landing.js
+++ src/public/Landing.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import WeForm from '../Components/LandingComponents/WeForm';
-import Groups from '../private/Groups';
 
 
 class Landing extends Component {
   render() {
     return (
 
-        <Groups/>
+        <WeForm/>
+
     );
   }
 }
