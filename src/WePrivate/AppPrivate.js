35e59b1a2bda5d2d378233d50420f745c595e03d
bismilahlf <bismilah.lf@gmail.com>
2018-09-06 20:15:32 +0200

    Add Thread styles

--- src/WePrivate/AppPrivate.js
+++ src/WePrivate/AppPrivate.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 // import Groups from './Groups';
 import Group from './Group';
 import WeList from '../Components/WeList';
-
+import Thread from './Thread';
 
 
 class AppPrivate extends Component {
   render() {
     return (
       <div className="AppPrivate">
-         <Group/>
+         {/* <Group/> */}
+         <Thread/>
       </div>
     );
   }
--- src/WePrivate/Thread.js
+++ src/WePrivate/Thread.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-
+import WeHeaderThread from '../Components/WeHeaderThread';
+import WeButtonOption from '../Components/WeButtonOption';
+import WeList from '../Components/WeList';
+import WeInputButton from '../Components/WeInputButton';
 
 class Thread extends Component {
   render() {
     return (
       <div className="App">
         <WeHeaderThread/>
+        <WeList
+          personName=
+        />
+        <WeInputButton/>
       </div>
     );
   }
