ca5833cc1ca227c50a5e4ef6b3ce30cab2b6c0cc
Raquel Villaverde <raquelv85@gmail.com>
2018-09-06 20:12:34 +0200

    add api call

--- src/App.js
+++ src/App.js
@@ -5,9 +5,33 @@ import AppPublic from './WePublic/AppPublic';
 import AppPrivate from './WePrivate/AppPrivate';
 
 class App extends Component {
+  constructor(){
+    super()
+  }
+
+  componentDidMount(){
+    fetch('http://adalab.string-projects.com/api/v1/sessions',{
+      method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+             body: JSON.stringify({
+                "nickname": 'adalab1',
+                "password": 'C12345678',
+            })
+    })
+    .then((response) => response.json({
+      
+    }))
+    .then((jsonskills) => {
+        console.log(jsonskills);
+    });
+
+  }
   render() {
     return (
       <div className="container-fluid">
+      <Login/>
          <AppPrivate/> 
         
         {/* <AppPublic/> */}
