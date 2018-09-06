2699fa7ad84b215f8caa3dd13406912eb8fce70c
raquelv85 <raquelv85@gmail.com>
2018-09-06 10:29:23 +0200

    modify scss and Login page

--- src/public/Landing.js
+++ src/public/Landing.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import WeHeader from '../Components/WeHeader';
 import WeHeaderThread from '../Components/WeHeaderThread';
 import WeForm from '../Components/LandingComponents/WeForm';
+import {Row, Col} from 'reactstrap';
 
 
 class Landing extends Component {
@@ -12,9 +13,20 @@ class Landing extends Component {
           <WeHeader/>
           </header>
           <main className="main--landing">
+          <Row>
+              <Col xl={{size: 4, offset: 2}} lg={{size: 6, offset: 0}} md={{size: 8, offset: 2}}>
+                     <div className="container--text-login">
             <h1 className="title--landing text-center">¡Hola de nuevo!</h1>
             <p className="subtitle--landing text-center">Escribe tu email y contraseña para entrar en We.</p>
-          <WeForm/>
+            </div>
+              </Col>
+              <Col xl="4" lg={{size: 6, offset: 0}} md={{size: 8, offset: 2}}>
+                  <WeForm/>
+              </Col>
+
+          </Row>
+     
+        
           </main>
         {/* <WeHeaderThread/> */}
         
