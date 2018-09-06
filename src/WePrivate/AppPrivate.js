05097b857cc0dbd1714e32fdb68843c56dfa9bec
Mariví <marivirh82@gmail.com>
2018-09-06 20:32:55 +0200

    reorder components on app private

--- src/WePrivate/AppPrivate.js
+++ src/WePrivate/AppPrivate.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import Groups from './Groups';
-import Group from './Group';
+import Groups from './Groups';
 import WeList from '../Components/WeList';
 
 
@@ -9,7 +9,7 @@ class AppPrivate extends Component {
   render() {
     return (
       <div className="AppPrivate">
-         <Group/>
+         <Groups/>
       </div>
     );
   }
