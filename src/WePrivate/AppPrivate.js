e18583e72f10e89e2dda1d1711898bbbd8f5e48f
bismilahlf <bismilah.lf@gmail.com>
2018-09-07 13:06:57 +0200

    Add changes

--- src/WePrivate/AppPrivate.js
+++ src/WePrivate/AppPrivate.js
@@ -3,21 +3,13 @@ import React, { Component } from 'react';
 import Groups from './Groups';
 import WeList from '../Components/WeList';
 import Thread from './Thread';
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 
 
 class AppPrivate extends Component {
   render() {
     return (
       <div className="AppPrivate">
-<<<<<<< HEAD
-         <Group/>
-=======
          <Groups/>
->>>>>>> master
          <Thread/>
       </div>
     );
