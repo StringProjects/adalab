1c19d8127d7a8cdea8642c8bbca22e7e46a1c698
rqlromo <rqlromo@gmail.com>
2018-09-11 11:32:51 +0200

    create PrivateRoute comoponent

---
+++ src/Components/PrivateComponents/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+      fakeAuth.isAuthenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{
+            pathname: '/login',
+            state: { from: props.location }
+          }} />
+    )} />
+)
+  
+
+export default PrivateRoute;
