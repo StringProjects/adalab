0f65bddf79c2787e7b6667616c2f8d4749ac41f4
raquelv85 <raquelv85@gmail.com>
2018-09-13 12:47:34 +0200

    modify styles

--- src/WePrivate/Group.js
+++ src/WePrivate/Group.js
@@ -50,7 +50,7 @@ class Group extends Component {
           </div>
         </div>
         <div className="wrapper-welist">
-        <ul>
+        <ul className="list--welist">
             {filterArray.map(function(group,i){
               return (
                 <Link to={`${rootRoute}${routeThread}`}>
@@ -61,7 +61,7 @@ class Group extends Component {
                     name={group.username}
                     date={group.date}
                     message={group.description}
-                    addAnswer="respuesta"
+                    addAnswer="Responder"
                     handleIdThread={handleIdThread}
                   />
                 </li>
