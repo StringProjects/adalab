900bc56f83809eeae42f9c93373a23918f845e3c
bismilahlf <bismilah.lf@gmail.com>
2018-09-17 17:54:12 +0200

    Revert change

--- src/WePrivate/Group.js
+++ src/WePrivate/Group.js
@@ -10,12 +10,18 @@ import profile from '../images/panda.jpg';
 
 class Group extends Component {
 
+  componentDidMount() {
+    const {fetchApiMessages} = this.props;
+    fetchApiMessages();
+  }
+
   render() {
     const {
       routePrivate,
       routePublic,
       routeGroup,
       location,
+      history,
       rootRoute,
       routeThread,
       handleIdThread,
