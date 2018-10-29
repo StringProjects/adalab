4f207f9a704bafe8ba393a88a8c650f786613adf
Raquel Romo <rqlromo@gmail.com>
2018-10-29 17:58:46 +0100

    install redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import { BrowserRouter as Router } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import '@fortawesome/fontawesome-free/css/all.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './scss/main.css';
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/main.css";
 
+import { Provider } from "react-redux";
+import configureStore from "./configureStore";
 
-ReactDOM.render(  
-  <HashRouter>
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
       <App />
-  </HashRouter>, 
-  document.getElementById('root')
+    </HashRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // ReactDOM.render(
---
+++ src/reducers/root.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+
+// import listCatalogue from './listCatalogue';
+// import listCart from './listCart';
+// import listFavourites from './listFavourites';
+
+//genera el store general
+const rootReducer = combineReducers({
+  // listCatalogue,
+  // listCart,
+  // listFavourites,
+})
+
+export default rootReducer
